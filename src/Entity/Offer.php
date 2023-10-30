e0c6ba5a7d3e90ba7726ff49d11951ef2bacb07d
Inès MAGANGA <i.maganga@yahoo.com>
2023-10-30 21:18:26 +0100

    CRUD Offer
    Using the make:crud command :
    
    Created the controller for offer table
    Created the entity for offer table
    Created the form for offer table
    Created the repository for offer table
    Created all twig templates for CRUD

---
+++ src/Entity/Offer.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OfferRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: OfferRepository::class)]
+class Offer
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 45)]
+    private ?string $name = null;
+
+    #[ORM\Column]
+    private ?int $allowed_accountant = null;
+
+    #[ORM\Column]
+    private ?float $price = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function setId(int $id): static
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): static
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAllowedAccountant(): ?int
+    {
+        return $this->allowed_accountant;
+    }
+
+    public function setAllowedAccountant(int $allowed_accountant): static
+    {
+        $this->allowed_accountant = $allowed_accountant;
+
+        return $this;
+    }
+
+    public function getPrice(): ?float
+    {
+        return $this->price;
+    }
+
+    public function setPrice(float $price): static
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+}
