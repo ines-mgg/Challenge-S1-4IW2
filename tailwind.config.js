c71c99c65269be271a6412d41e55f23cbf06cc7f
Inès MAGANGA <i.maganga@yahoo.com>
2023-11-27 22:00:14 +0100

    updated header and started section of landing page

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,15 +9,12 @@ module.exports = {
         colors: {
             'light-dark': '#1C1C1A',
             'light-white': '#F9F9F9',
+            'beige': '#F6F5EF',
         },
         // add new height of 7.125rem
         height: {
             '7.125': '1.75rem',
         },
-        // add new margin
-        margin: {
-          '610px': '1100px'
-        }
     },
   },
   plugins: [],
