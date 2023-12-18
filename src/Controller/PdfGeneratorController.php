c2b9f70f1d9d69d6b7a6c51f163209e4fc7a776a
Estelle <estelle272001@gmail.com>
2023-12-18 10:16:50 +0100

    Start pdf

---
+++ src/Controller/PdfGeneratorController.php
@@ -0,0 +1,63 @@
+<?php
+
+namespace App\Controller;
+
+use Dompdf\Dompdf;
+use Dompdf\Options;
+use http\Client\Request;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
+use Symfony\Component\Mime\Email;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Controller\MailerController;
+use Symfony\Component\Mailer\MailerInterface;
+
+
+class PdfGeneratorController extends AbstractController
+{
+    #[Route('/pdf/generator', name: 'app_pdf_generator')]
+    public function index(MailerInterface $mailer,Request $request): Response
+    {
+
+        $data = [
+            'title' => "Welcome to our PDF Test"
+        ];
+        $form = $this->createForm(ContactType::class, $data);
+        $pdfOptions = new Options();
+        $pdfOptions->set('defaultFont', 'Arial');
+        $dompdf = new Dompdf($pdfOptions);
+        $html = $this->renderView('pdf_generator/index.html.twig', [
+            'title' => "Welcome to our PDF Test",
+            'controller_name' => 'PdfGeneratorController NOn',
+        ]);
+
+        $dompdf->loadHtml($html);
+        $dompdf->setPaper('A4', 'portrait');
+        $dompdf->render();
+        $email = (new Email())
+            ->from('no_reply@facturo.com') // Sender
+            ->to('estelle272001@gmail.com')// Recipient
+            ->subject('Devis') // Subject
+            ->text('Sending Devis is fun again!') // Content
+            ->html('<p>This is you devis ok !</p>') // Content
+            ->attach($dompdf->output(), 'mypdf.pdf', 'application/pdf');
+
+        try {
+            $mailer->send($email);
+        return $this->render('pdf_generator/index.html.twig', [
+            'controller_name' => 'PdfGeneratorController',
+            'title' => "Welcome to our PDF Test",
+        ]);
+        } catch (TransportExceptionInterface $e) {
+            var_dump($e->getMessage());
+        }
+        return $this->render('pdf_generator/index.html.twig', [
+            'controller_name' => 'PdfGeneratorController NOn',
+            'title' => "Welcome to our PDF Test",
+
+        ]);
+//     return new Response('Email sent successfully');
+    }
+
+}
