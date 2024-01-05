d8c2468ade3f52f5b110f03c7e2d03f482833740
Estelle <estelle272001@gmail.com>
2024-01-05 21:14:58 +0100

    fix bugs

--- assets/showcase/showcase.js
+++ assets/showcase/showcase.js
@@ -7,7 +7,6 @@
 
 // any CSS you import will output into a single css file (showcase.css in this case)
 import './styles/showcase.css';
-import { startStimulusApp } from '@symfony/stimulus-bridge';
 
 export const showcase = startStimulusApp(require.context(
     '@symfony/stimulus-bridge/lazy-controller-loader!../controllers',
--- src/Controller/CompanyController.php
+++ src/Controller/CompanyController.php
@@ -11,7 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/company', name: 'app_company_')]
+#[Route('/company')]
 class CompanyController extends AbstractController
 {
     #[Route('/', name: 'app_company_index', methods: ['GET'])]
@@ -22,7 +22,7 @@ class CompanyController extends AbstractController
         ]);
     }
 
-    #[Route('/new', name: 'app_company_new', methods: ['GET', 'POST'])]
+    #[Route('/newCompany', name: 'app_company_new', methods: ['GET', 'POST'])]
     public function new(Request $request, EntityManagerInterface $entityManager): Response
     {
         $company = new Company();
