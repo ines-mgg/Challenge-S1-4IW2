f9da5b4261259ab6d57958eaf6f043914381e6f2
ines-mgg <i.maganga@yahoo.com>
2024-02-05 19:21:10 +0100

    cookie for dark mode

--- assets/showcase/showcase.js
+++ assets/showcase/showcase.js
@@ -46,16 +46,29 @@ window.addEventListener('load', () => {
     <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256"><path fill="#fcd34d" d="M120 40V16a8 8 0 0 1 16 0v24a8 8 0 0 1-16 0m8 24a64 64 0 1 0 64 64a64.07 64.07 0 0 0-64-64m-69.66 5.66a8 8 0 0 0 11.32-11.32l-16-16a8 8 0 0 0-11.32 11.32Zm0 116.68l-16 16a8 8 0 0 0 11.32 11.32l16-16a8 8 0 0 0-11.32-11.32M192 72a8 8 0 0 0 5.66-2.34l16-16a8 8 0 0 0-11.32-11.32l-16 16A8 8 0 0 0 192 72m5.66 114.34a8 8 0 0 0-11.32 11.32l16 16a8 8 0 0 0 11.32-11.32ZM48 128a8 8 0 0 0-8-8H16a8 8 0 0 0 0 16h24a8 8 0 0 0 8-8m80 80a8 8 0 0 0-8 8v24a8 8 0 0 0 16 0v-24a8 8 0 0 0-8-8m112-88h-24a8 8 0 0 0 0 16h24a8 8 0 0 0 0-16"/></svg>
     `;
 
-    switchButton.addEventListener('click', toggleTheme);
 
-    function toggleTheme() {
-    isDarkmode = !isDarkmode;
-    localStorage.setItem("isDarkmode", isDarkmode);
-    switchTheme();
+    const cookieValue = document.cookie
+    .split("; ")
+    .find((row) => row.startsWith("themeEnable"))
+    ?.split("=")[1];
+
+    switchButton.addEventListener('click', toggleTheme(cookieValue));
+
+
+    function toggleTheme(cookieValue) {
+    console.log("?", cookieValue);
+    if(cookieValue == 'false') {
+        document.cookie = "themeEnable=true;";
+        switchTheme('true');
+    }else {
+        document.cookie = "themeEnable=false;";
+        switchTheme('false');
     }
+    }
+    
 
-    function switchTheme() {
-        if (isDarkmode && localStorage.isDarkmode) {
+    function switchTheme(cookieValue) {
+        if (cookieValue == 'true') {
             document.documentElement.classList.add('dark')
             switchToggle.classList.remove("-translate-x-2");
             switchToggle.classList.add("translate-x-full");
@@ -76,7 +89,7 @@ window.addEventListener('load', () => {
         }
     }
 
-    switchTheme();
+    switchTheme(cookieValue);
 
     /* DARK MODE */
 });
