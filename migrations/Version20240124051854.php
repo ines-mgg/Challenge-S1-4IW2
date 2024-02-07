d867a42f293502384869a8af267d3f7cf22a812f
ines-mgg <i.maganga@yahoo.com>
2024-02-07 15:21:27 +0100

    fix database

--- migrations/Version20240124051854.php
+++ migrations/Version20240124051854.php
@@ -20,8 +20,8 @@ final class Version20240124051854 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE invoice ADD price DOUBLE PRECISION NOT NULL');
-        $this->addSql('ALTER TABLE "user" ALTER company_id SET NULL');
+        $this->addSql('ALTER TABLE invoice ALTER COLUMN price TYPE DOUBLE PRECISION');
+        $this->addSql('ALTER TABLE "user" ALTER company_id SET DEFAULT NULL');
         $this->addSql('ALTER TABLE "user" ALTER lastname SET NOT NULL');
         $this->addSql('ALTER TABLE "user" ALTER firstname SET NOT NULL');
         $this->addSql('ALTER TABLE "user" ALTER created_at DROP DEFAULT');
@@ -34,7 +34,6 @@ final class Version20240124051854 extends AbstractMigration
     {
         // this down() migration is auto-generated, please modify it to your needs
         $this->addSql('CREATE SCHEMA public');
-        $this->addSql('ALTER TABLE invoice DROP price');
         $this->addSql('ALTER TABLE "user" ALTER company_id DROP NOT NULL');
         $this->addSql('ALTER TABLE "user" ALTER lastname DROP NOT NULL');
         $this->addSql('ALTER TABLE "user" ALTER firstname DROP NOT NULL');
