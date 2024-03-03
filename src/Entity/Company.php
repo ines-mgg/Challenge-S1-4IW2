903a368fdcb532fe5199401af612bc386f56a7bc
Victor GRANDIN <78478328+grdnvictor@users.noreply.github.com>
2024-03-03 15:07:38 +0100

    added company creation if empty and removed messenger

--- src/Entity/Company.php
+++ src/Entity/Company.php
@@ -37,13 +37,13 @@ class Company
 
     #[ORM\OneToMany(mappedBy: 'company', targetEntity: Customer::class)]
     private Collection $customers;
-    #[ORM\Column(length: 14)]
+    #[ORM\Column(length: 14, nullable: true)]
     private ?string $siret = null;
 
     #[ORM\Column(length: 50, nullable: true)]
     private ?string $tva = null;
 
-    #[ORM\Column(length: 50)]
+    #[ORM\Column(length: 50, nullable: true)]
     private ?string $head_office;
 
     public function __construct()
